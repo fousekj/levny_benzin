6b77003e8fba9d8a392e06fc237044d059d94e85
Jiří Fousek <jirfousek@gmail.com>
2022-03-17 12:55:14 +0100

    routování

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GasStationController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,5 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::resource('/gasStation', GasStationController::class);
-//Route::get('', WelcomeController);
+Route::get('', WelcomeController::class);
 //Route::resource('/gasStation/{id}', GasStationController::class);
