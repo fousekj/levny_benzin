106c726fe6060f345cc2624eefff9cce0c4a267d
Jiří Fousek <jirfousek@gmail.com>
2022-03-17 12:52:50 +0100

    GasStationController.php - create, store, edit, show, index

--- app/Http/Controllers/GasStationController.php
+++ app/Http/Controllers/GasStationController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\GasStation\StoreRequest;
+use App\Models\Company;
 use App\Models\GasStation;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
+use mysql_xdevapi\Exception;
 
 class GasStationController extends Controller
 {
@@ -31,22 +37,38 @@ class GasStationController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        return view('gasStation.create', [
+            'gasStations' => GasStation::all(),
+            'companies' => Company::all()
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @param StoreRequest $request
+     * @return RedirectResponse
+     * @throws ValidationException
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        GasStation::create([
+            'company_id' => $request->company_id,
+            'street' => $request->street,
+            'city' => $request->city,
+            'priceDiesel' => $request->priceDiesel ?? '0.0',
+            'priceDieselSpecial' => $request->priceDieselSpecial ?? '0.0',
+            'pricePetrol' =>$request->pricePetrol ?? '0.0',
+            'pricePetrolSpecial' =>$request->pricePetrolSpecial ?? '0.0',
+            'priceCNG' =>$request->priceCNG ?? '0.0',
+            'priceLPG' =>$request->priceLPG ?? '0.0',
+        ]);
+
+        return redirect()->route('gasStation.index');
     }
 
     /**
@@ -58,8 +80,8 @@ class GasStationController extends Controller
     public function show($id)
     {
         return view('gasStation.show', [
-            'gasStation' => GasStation::findOrFail($id),
-            'company' => GasStation::findOrFail($id)->company
+            'gasStation' => GasStation::query()->findOrFail($id),
+            'company' => GasStation::query()->findOrFail($id)->company
         ]);
     }
 
@@ -67,11 +89,11 @@ class GasStationController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function edit($id)
     {
-        //
+        return view('gasStation.edit');
     }
 
     /**
