3f7c0263cf7a5a495762b6223c8241c7aa1dc5bf
Jiří Fousek <jirfousek@gmail.com>
2022-04-17 22:38:05 +0200

    Přidání nových rout pro Admina

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\GasStationController;
 use App\Http\Controllers\UserControler;
@@ -36,21 +37,33 @@ Route::post('contact', [ContactController::class, 'send'])->name('contact.send')
 /**
  * Routy pro Admin
  */
-Route::get('/admin', function () {
-    return view('admin_dashboard');
-})->middleware(['auth', 'admin']);
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function (){
+    Route::get('/', [AdminController::class, 'home'])->name('admin.home');
+});
+
+Route::middleware(['auth', 'admin'])->prefix('admin/cerpaci-stanice')->group(function (){
+    Route::get('/list', [AdminController::class, 'list'])->name('admin.list');
+    Route::get('/show/{id}', [AdminController::class, 'show'])->name('admin.show');
+    Route::get('/edit/{id}', [AdminController::class, 'edit'])->name('admin.edit');
+    Route::put('/update/{id}', [GasStationController::class, 'update'])->name('gasStation.edit');
+    Route::get('/create', [GasStationController::class, 'create'])->name('gasStation.create');
+    Route::put('/create', [GasStationController::class, 'store'])->name('gasStation.store');
+});
 
 /**
  * Routy pro User
  */
-Route::middleware(['auth', 'user'])->prefix('user')->group(function () {
+Route::middleware(['auth', 'user'])->prefix('user')->group(function (){
     Route::get('/', [UserControler::class, 'home'])->name('user.home');
+});
+
+Route::middleware(['auth', 'user'])->prefix('user/cerpaci-stanice')->group(function () {
     Route::get('/list', [UserControler::class, 'list'])->name('user.list');
     Route::get('/show/{id}', [UserControler::class, 'show'])->name('user.show');
     Route::get('/edit/{id}', [UserControler::class, 'edit'])->name('user.editPrices');
+    Route::put('/update/{id}', [GasStationController::class, 'update'])->name('gasStation.updatePrices');
 
 });
 
-Route::put('cerpaci-stanice/update/{id}', [GasStationController::class, 'update'])->name('gasStation.editPrices');
 
 Auth::routes(['verify' => true]);
